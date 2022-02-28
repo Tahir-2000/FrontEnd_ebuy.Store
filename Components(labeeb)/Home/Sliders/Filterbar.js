86821fa20e5a6fbb98cd3c4dd3ab2f0dabd84a64
Tahir-2000 <96041989+Tahir-2000@users.noreply.github.com>
2022-02-28 15:03:18 +0500

    Home component
    Home Component with filter bar, sliders and product catalogue

---
+++ Components(labeeb)/Home/Sliders/Filterbar.js
@@ -0,0 +1,42 @@
+// import React from "react";
+// import Navbar from "../../Navbar/Navbar.js";
+// import Sale from "../../Sale/Sale.js";
+// import {Link} from "react-router-dom";
+// import {Dropdown} from "react-bootstrap";
+// import  "./Home.css"
+
+// const filter =()=>{
+//    const style={
+//        textDecoration:"none",
+//        backgroundColor:"#FFFFFF",
+//        color:"black" ,
+//        justifyContent:"centered"   
+//    };
+//     return(
+//         <div className="container-fluid  bg-secondary">
+//         <div className="row">
+//             <div  className="  col-md-3 col-md-first bg-light">
+//                 <div className="col-4 col-md-3">
+//                     <li className="list-group-item"><Link style={style} to="/AllProducts">All</Link> </li>
+//                 </div>
+//                 <div className="col-4 col-md-3">
+                   
+//                             <Dropdown>
+//                             <Dropdown.Toggle style={style} id="dropdown-basic">
+//                                 Dropdown Button
+//                             </Dropdown.Toggle>
+    
+//                             <Dropdown.Menu>
+//                                 <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+//                                 <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+//                                 <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+//                             </Dropdown.Menu>
+//                             </Dropdown>
+                   
+//                 </div>
+                
+//             </div>
+//         </div>
+//        </div>
+//     );
+// }
