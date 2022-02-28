2415879e2acacda726690e1af0056f16310a24d0
shameerk2888 <96041911+shameerk2888@users.noreply.github.com>
2022-02-28 14:58:06 +0500

    Update app.js

--- Backend(Shameer)/app.js
+++ Backend(Shameer)/app.js
@@ -1,9 +1,8 @@
 const express = require('express');
-const cors = require('cors')
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 
 app.use('/api/v1/products',productRoutes);
