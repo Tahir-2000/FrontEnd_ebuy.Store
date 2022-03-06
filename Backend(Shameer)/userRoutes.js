513281ac756d3722acf9f5b3c393d077ea83c916
shameerk2888 <96041911+shameerk2888@users.noreply.github.com>
2022-03-06 19:19:31 +0500

    Add files via upload

---
+++ Backend(Shameer)/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
+
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/changePassword/:token', authController.changePassword);
+
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
+module.exports = router;
