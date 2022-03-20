966575ce1da132f1aef2fb6a305815e26d8d79a5
shameerk2888 <96041911+shameerk2888@users.noreply.github.com>
2022-03-20 22:28:42 +0500

    Add files via upload

---
+++ Backend(Shameer)/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json()); // a method inbuilt in express to recognize the incoming Request Object as a JSON Object
+
+app.use('/api/v1/products', productRoutes);
+app.use('/api/v1/users', userRoutes);
+
+
+module.exports = app;
