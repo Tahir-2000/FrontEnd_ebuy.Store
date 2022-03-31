21cbbb5a67b21512d96cd9a33655c3a1fbc94b55
shameerk2888 <96041911+shameerk2888@users.noreply.github.com>
2022-03-31 16:17:53 +0500

    Add files via upload

---
+++ Backend(Shameer)/controllers/authController.js
@@ -0,0 +1,380 @@
+const jwt = require('jsonwebtoken');
+const User = require('./../models/userModel');
+const catchAsync = require('../utilits/catchAsync');
+const AppError = require('./../utilits/appError');
+const crypto = require('crypto');
+const { promisify } = require('util');
+const nodemailer = require('nodemailer')
+
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+}
+const createSendToken = (res, user, status) => {
+    const token = signToken(user._id)
+
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    };
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+
+
+    res.status(status).json({
+        status: 'success',
+        jwt: token,
+        data: {
+            user
+        }
+    })
+}
+
+exports.protect = catchAsync(async(req, res, next) => { // protect() middleware function for protecting routes by giving access to route for loggedIn users only
+    let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+    console.log(token);
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET); // catchAsync will auto send err to global error handler if accurs like expired or invalid JWT errrs
+
+    const currentUser = await User.findById(decoded.id);
+
+    if (!currentUser) {
+        return next(new AppError('The user of token not longer exist', 401))
+    }
+    //  Check if user changed password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next(
+            new AppError('User recently changed password! Please log in again.', 401)
+        );
+    }
+
+    req.user = currentUser; // must place current user in req.user so routes after that middleware can acces to currently loggedin user by req.user
+    next(); // must place next() at the end bcz this is a middleare so the next comming route or middleware can run
+});
+
+exports.signup = catchAsync(async(req, res, next) => {
+
+    const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+
+    });
+
+    createSendToken(res, user, 201);
+
+});
+
+exports.signin = catchAsync(async(req, res, next) => {
+
+    const { password, email } = req.body;
+    if (!password || !email) {
+        return next(new AppError('Please provide password and email ', 400))
+    }
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+        return next(new AppError('Incorrect Password or Email', 401))
+
+    }
+    createSendToken(res, user, 200);
+
+});
+
+exports.forgetPassword = catchAsync(async(req, res, next) => {
+    const { email } = req.body;
+    if (!email) return next(new AppError('Please enter email Address', 400))
+    const user = await User.findOne({ email: email });
+    if (!user) return next(new AppError(`There is no user with ${email} email.. Please provide valid Email Addrees`, 401));
+
+    const resetToken = await user.createPasswordResetToken();
+    await user.save({ validateBeforSave: false }); // preventing every kind of validation and mongoose middleware to run for this doc save().
+
+    const resetURL = ` http://localhost:3001/ChangePassword/${resetToken}`;
+    const message = `Forget your Password? If yes! Then click on the given Link and set up your new password: ${resetURL}. \n
+    If you did not forget password, Please ignore this Email`;
+
+    try {
+        const smtpConfig = {
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // use SSL
+            auth: {
+                user: `${process.env.EMAIL_USERNAME}`,
+                pass: `${process.env.EMAIL_PASSWORD}`
+            }
+        };
+        const transporter = nodemailer.createTransport(smtpConfig);
+
+        const mailOptions = {
+            from: 'ebuystore37@gmail.com',
+            to: user.email,
+            subject: 'Your password reset token "only valid for 10 minutes"',
+            text: message
+        }
+
+        transporter.sendMail(mailOptions, async(err, data) => {
+            if (err) {
+                console.log(err)
+                user.passwordResetToken = undefined;
+                user.passwordResetExpires = undefined;
+                await user.save({ validateBeforSave: true });
+                return next(new AppError('There was an error while sending email. Try again later', 500))
+            } else {
+                console.log(data)
+                res.status(200).json({
+                    status: 'success',
+                    message: 'password reset instructions send to given email, Please go and check Email on urgent basis!'
+                })
+            }
+
+        })
+    } catch (err) {
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({ validateBeforSave: true });
+        return next(new AppError('There was an error while sending email. Try again later', 500))
+    }
+});
+
+exports.signout = catchAsync(async(req, res, next) => {
+
+
+    try {
+
+        console.log('successfully loged out')
+        res.status(200).json({
+            status: 'success',
+            message: 'successfully loged oput'
+        })
+    } catch (err) {
+        console.log(err)
+    }
+
+})
+
+exports.setAddress = catchAsync(async(req, res, next) => {
+    let Address = {
+        name: req.body.name,
+        contact: req.body.contact,
+        address: req.body.address,
+        cityName: req.body.cityName,
+        provinceName: req.body.provinceName,
+    };
+
+    if (!Address) {
+        return new AppError('please fill address books', 400)
+    }
+    console.log(Address)
+    const user = await User.findByIdAndUpdate(req.user, { address: Address }, { useFindAndModify: false });
+    if (!user) {
+        return new AppError('cannot add user address', 500)
+    }
+    res.status(201).json({
+        status: 'success',
+        user
+    })
+});
+
+exports.getUser = catchAsync(async(req, res, next) => {
+
+
+    const user = await User.findById(req.user).populate({
+        path: 'orderedProducts',
+        select: '-__v -password -confirmPassword  -passwordResetToken -passwordResetExpires'
+    });
+    if (!user) {
+        return new AppError('user didnot exist anymore', 500)
+    }
+    res.status(201).json({
+        status: 'success',
+        user
+    })
+})
+
+exports.setUserOrder = catchAsync(async(req, res, next) => {
+    let previousTotal = req.body.previousTotal;
+    let subTotal = req.body.subTotal;
+    let shippingFee = req.body.shippingFee;
+    let productIDs = [...req.body.products];
+    let to = `lat712000 , ${req.user.email}`;
+    console.log(to)
+    console.log(req.body)
+    const userData = await User.findById(req.user)
+
+    const userOlderOrders = [...userData.orderedProducts]
+
+
+    for (let i = 0; i < userOlderOrders.length; i++) {
+        productIDs.push(userOlderOrders[i]);
+
+    }
+    console.log(productIDs)
+    const user = await User.findByIdAndUpdate(req.user, { orderedProducts: productIDs }, { useFindAndModify: false });
+    if (!user) {
+        return new AppError('user didnot exist anymore', 500)
+    } else {
+
+        try {
+            const smtpConfig = {
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true, // use SSL
+                auth: {
+                    user: `${process.env.EMAIL_USERNAME}`,
+                    pass: `${process.env.EMAIL_PASSWORD}`
+                }
+            };
+            let transporter = nodemailer.createTransport(smtpConfig);
+
+            const mailOptions1 = { //mail to admin
+                from: 'ebuystore37@gmail.com',
+                to: 'lat712000@gmail.com',
+                subject: 'New Order',
+                text: `Customer Name: ${user.name}   Id: ${user._id}
+                   Order Details:
+                   1) Previouse  non-Delivered Order with '${userOlderOrders.length}' items: Rs. ${previousTotal}.
+                   2) New  Order with '${req.body.products.length}' items: Rs. ${subTotal}.
+                   3) Shipping Charges: Rs. ${shippingFee}
+                   -----------------------------------------------------------------------------
+                      Total: Rs. ${subTotal+shippingFee+previousTotal}
+                   -----------------------------------------------------------------------------`
+
+            }
+
+            const mailOptions2 = { // mail to customer
+                from: 'ebuystore37@gmail.com',
+                to: user.email,
+                subject: 'Order Details',
+                text: `Thanks for Shopping at wwww.onlinegrocerymart.store
+
+                   Order Details:
+                   Customer Name: ${user.name}.
+                   1) Previouse  non-Delivered Order with '${userOlderOrders.length}' items: Rs. ${previousTotal}.
+                   2) New  Order with '${req.body.products.length}' items: Rs. ${subTotal}.
+                   3) Shipping Charges: Rs. ${shippingFee}
+                   -----------------------------------------------------------------------------
+                      Total: Rs. ${subTotal+shippingFee+previousTotal}
+                   -----------------------------------------------------------------------------`
+
+            }
+
+            await transporter.sendMail(mailOptions1)
+            await transporter.sendMail(mailOptions2)
+
+        } catch (err) {
+            return res.status(201).json({
+                status: 'success',
+                user
+            })
+        }
+        res.status(201).json({
+            status: 'success',
+            user
+        })
+    }
+
+});
+
+exports.allOrders = catchAsync(async(req, res, next) => {
+    const userID = req.body.ID;
+    const currentUser = req.user;
+    console.log('this is current user', currentUser)
+    let users;
+    if (userID === undefined || userID === "" || userID === " ") {
+        users = await User.find().populate({
+            path: 'orderedProducts',
+            select: '-__v -password -confirmPassword  -passwordResetToken -passwordResetExpires'
+        })
+    } else if (userID !== undefined && userID !== "" && userID !== " ") {
+        if (userID === 'LoggedInUser') {
+            users = await User.find({ _id: currentUser._id }).populate({
+                path: 'orderedProducts',
+                select: '-__v -password -confirmPassword  -passwordResetToken -passwordResetExpires'
+            })
+        } else {
+            users = await User.find({ _id: userID }).populate({
+                path: 'orderedProducts',
+                select: '-__v -password -confirmPassword  -passwordResetToken -passwordResetExpires'
+            })
+        }
+    }
+    console.log(users)
+    if (!users) {
+        return new AppError('cannot get any order', 204)
+    }
+    res.status(201).json({
+        status: 'success',
+        currentUser,
+        users
+
+    })
+})
+
+exports.updateOrdersList = catchAsync(async(req, res, next) => {
+
+
+    let productIDs = [...req.body.productList]
+    console.log(productIDs)
+
+    const user = await User.findByIdAndUpdate(req.body.userID, { orderedProducts: productIDs }, { useFindAndModify: false }).populate({
+        path: 'orderedProducts',
+        select: '-__v -password -confirmPassword  -passwordResetToken -passwordResetExpires'
+    });
+    if (!user) {
+        return new AppError('user didnot exist anymore', 500)
+    }
+    res.status(201).json({
+        status: 'success',
+        user
+    })
+})
+
+exports.changePassword = catchAsync(async(req, res, next) => {
+    console.log(req.body)
+    const token = req.body.resetToken;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    const hashedToken = crypto.createHash('sha256').update(token).digest('hex');
+    const user = await User.findOne({ passwordResetToken: hashedToken, passwordResetExpires: { $gt: Date.now() } })
+    console.log(Date.now())
+    console.log(user)
+    if (!user) {
+        return next(new AppError('Token is invalid or expired', 400))
+    }
+    user.password = password;
+    user.confirmPassword = confirmPassword;
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save() // we haven't turned of validation bcz we want to validate confirm password and other validation by save middleware
+
+    createSendToken(res, user, 200);
+
+})
+
+exports.changeEmail = catchAsync(async(req, res, next) => {
+    const oldEmail = req.body.oldEmail;
+    const newEmail = req.body.newEmail;
+    const password = req.body.password;
+    if (req.user.email === oldEmail) {
+        const user = await User.findOneAndUpdate({ email: oldEmail }, { email: newEmail }, { useFindAndModify: false })
+        if (!user) {
+            return next(new AppError('cannot update email!', 400))
+        }
+        res.status(200).json({
+            status: 'success',
+            user
+        })
+    } else {
+        return next(new AppError('please provide correct email address that you used to login before!', 401))
+    }
+})
---
+++ Backend(Shameer)/controllers/productController.js
@@ -0,0 +1,157 @@
+const Product = require('./../models/productModel');
+const catchAsync = require('../utilits/catchAsync');
+const AppError = require('./../utilits/appError');
+
+sendResponce = (res, product, statusCode) => {
+
+    res.status(statusCode).json({
+        status: 'success',
+        product
+    });
+}
+
+exports.addProduct = catchAsync(async(req, res, next) => {
+
+    let { pTitle, pCetegory, pPrice, pImagePath, pDescription, pStock, pEdible, pOnSale, pOldPrice } = req.body;
+    if (pOldPrice === '' || pOldPrice === null) {
+        pOldPrice = ' '
+    }
+
+    if (!pTitle || !pCetegory || !pPrice || !pImagePath || !pStock) {
+        return next(new AppError('please fill all required fields', 400))
+    }
+    const product = await Product.create({
+        pPrice: pPrice,
+        pTitle: pTitle,
+        pCetegory: pCetegory,
+        pImagePath: pImagePath,
+        pDescription: pDescription,
+        pStock: pStock,
+        pEdible: pEdible,
+        pOnSale: pOnSale,
+        pOldPrice: pOldPrice
+    });
+
+    sendResponce(res, product, 201);
+})
+exports.showProducts = catchAsync(async(req, res, next) => {
+
+    let products = await Product.find(req.body).sort([
+        ['pTitle', 1]
+    ]);
+    if (!products) {
+        return (new AppError('can not get the products ', 500))
+    }
+
+
+    sendResponce(res, products, 200)
+});
+
+exports.editProducts = catchAsync(async(req, res, next) => {
+    const requestBody = {...req.body } // hard copy of req.body
+    if (requestBody.pOldPrice === '') {
+        requestBody.pOldPrice = ' '
+    }
+    const keys = Object.keys(requestBody) // getting out all property names as keys  of object request object
+        // We already filtered out those properties from object to be send here from front End that have empty data ""
+    keys.forEach((key, index) => { // looping out object by its property keys
+        if (requestBody[key] === '' || requestBody[key] === null) { // important '' != ' '
+            delete requestBody[key]; // deleting property as well as its value which should be either null  or 'withoutSpace' means data that is empty
+        } // 'withSpace' or "withSpace" means it is string data not empty
+    })
+
+    let products = await Product.findByIdAndUpdate(req.body._id, requestBody, { useFindAndModify: false });
+    if (!products) {
+        return (new AppError('can not get the products ', 500))
+    }
+    sendResponce(res, products, 200)
+});
+
+exports.getProducts = catchAsync(async(req, res, next) => {
+    let products;
+    if (req.body) {
+
+        let variable = req.body;
+        let limit = variable.limit ? parseInt(variable.limit) : 100;
+        let skip = parseInt(variable.skip);
+        let term = variable.searchTerms;
+
+        if (term) {
+            products = await Product.find({ pStock: { $gt: 0 } })
+                .find({ $text: { $search: term } })
+                .sort({ pTitle: 1 })
+
+        } else {
+            products = await Product.find({ pStock: { $gt: 0 } })
+                .sort({ pTitle: 1 })
+                .skip(skip)
+                .limit(limit)
+        }
+    } else {
+        products = await Product.find({ pStock: { $gt: 0 } })
+    }
+
+    sendResponce(res, products, 200)
+});
+
+exports.getSaleProducts = catchAsync(async(req, res, next) => {
+    const products = await Product.find({ pStock: { $gt: 0 } }).where('pOnSale').in('onSale')
+    if (!products) {
+        return (new AppError('no product on sale ', 204))
+    }
+    sendResponce(res, products, 200)
+})
+
+exports.getCetegoryProducts = catchAsync(async(req, res, next) => {
+
+
+    const [cetegory1, cetegory2] = req.body.cetegory.split(',');
+
+    const cetegory = [
+        cetegory1,
+        cetegory2
+    ]
+
+    if (!cetegory) {
+        return new AppError('please select cetegory', 400)
+    }
+    const products = await Product.find({ pStock: { $gt: 0 } }).where('pCetegory').in(cetegory)
+
+
+    if (!products) {
+        return new AppError(`no product for ${cetegory} cetegory`, 204)
+
+    }
+    sendResponce(res, products, 200)
+})
+exports.getCartProducts = catchAsync(async(req, res, next) => {
+
+    if (!req.body.id) {
+        return new AppError('please select cetegory', 400)
+    }
+    const products = await Product.find().where('_id').in(req.body.id)
+
+    if (!products) {
+        return new AppError(`no product found of given id`, 204)
+
+    }
+    sendResponce(res, products, 200)
+})
+
+exports.editProductsCount = catchAsync(async(req, res, next) => {
+
+    let productIDs = [...req.body.products]
+    console.log(productIDs)
+
+    let products = await Product.find().where('_id').in(productIDs);
+
+    await products.map(async(product) => {
+
+        count = product.pStock - productIDs.filter((item) => item == product._id).length;
+        console.log(count)
+        return await Product.findByIdAndUpdate(product._id, { pStock: count }, { useFindAndModify: false })
+    })
+    res.status(200).json({
+        status: 'success',
+    })
+})
